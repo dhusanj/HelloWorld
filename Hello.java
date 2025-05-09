be1d1727e8ad6d7e7484dc2a3a14c18845bda8a7
Sanjiv Dhungel <sanjivdhungel2022@gmail.com>
2025-05-09 18:10:50 -0500

    Friend name added.

--- Hello.java
+++ Hello.java
@@ -10,10 +10,13 @@ public class Hello {
         String year = "2025";
         String firstDaughter = "Subhi";
         String secondDaughter = "Maya";
+        String friendName = "Dinesh";
+
         System.out.println(day);
         System.out.println(month);
         System.out.println(year);
         System.out.println(firstDaughter);
         System.out.println(secondDaughter);
+        System.out.println(friendName);
     }
 }
