005a1673df64f4be4622a11f82afcfd112e332ac
Sanjiv Dhungel <sanjivdhungel2022@gmail.com>
2025-05-09 16:45:34 -0500

    second commit

--- Hello.java
+++ Hello.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Hello {
+
+    public static void main(String[] args) {
+        testing();
+    }
+
+    public static void testing() {
+        String day = "Friday";
+        String month = "May";
+        System.out.println(day);
+        System.out.println(month);
+    }
+
+
 }
