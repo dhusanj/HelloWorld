825e222f27dafe49a862a1a2eefd0c7fdb9d5611
Sanjiv Dhungel <sanjivdhungel2022@gmail.com>
2025-05-09 17:10:41 -0500

    updated as per PR feedback

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
     public static void testing() {
         String day = "Friday";
         String month = "May";
-        String year = "2020";
+        String year = "2025";
         System.out.println(day);
         System.out.println(month);
         System.out.println(year);
